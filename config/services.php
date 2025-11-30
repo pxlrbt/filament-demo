18c38526182fd91f7d1ad012a933b58ee65d3055
Dennis Koch <info@pixelarbeit.de>
2025-11-30 17:25:29 +0100

    Add Changelog Pro Config

--- config/services.php
+++ config/services.php
@@ -30,4 +30,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'github' => [
+        'repo' => env('GITHUB_REPO'),
+        'token' => env('GITHUB_TOKEN'),
+    ]
+
 ];
